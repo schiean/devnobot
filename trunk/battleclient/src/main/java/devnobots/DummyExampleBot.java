ecdeaaf8506a5d293ba2324e403df04d933fa142
arjen.van.schie@gmail.com <arjen.van.schie@gmail.com@be7e9fc5-4206-afe5-eb4c-f1bd711e812f>
2012-01-06 14:50:56 +0000

    first client version

---
+++ trunk/battleclient/src/main/java/devnobots/DummyExampleBot.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2011 AJG van Schie
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package devnobots;
+import java.awt.Color;
+import java.util.Random;
+
+import rest.service.types.Action;
+
+
+public class DummyExampleBot implements Runnable{
+	
+	private final ClientApi api;
+	private final Random randomGenerator = new Random();
+	private final String name;
+	private final String color;
+
+	public DummyExampleBot(final String host,final String name, final String color){
+		this.api = new ClientApi(host, false);
+		this.name = name;
+		this.color = color;
+		System.out.println("name:"+name+" color:"+color+" ("+Color.decode(color).toString()+")");
+	}	
+	
+	@Override
+	public void run(){
+		api.readLevel();
+		api.createPlayer(name, color);
+		api.readPlayers();
+		
+		while(true){
+			Action action = Action.values()[randomGenerator.nextInt(Action.values().length)];
+			api.addAction(action, name);
+			//api.readWorldStatus();
+		
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}			
+		}
+	}
+	
+}
